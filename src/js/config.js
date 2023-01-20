c9c3c93fd9464f06512f8c332e544104d5703fc7
bakocn <nikola.bakoc.mes@gmail.com>
2023-01-20 19:03:02 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'abafc191-e8fc-408f-a31f-f9a8fd8a23bd';
+export const MODAL_CLOSE_SEC = 1.5;
